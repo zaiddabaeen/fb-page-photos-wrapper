3b2af452b0a71f938321992471db63b388015af4
zaiddabaeen <redtroops@Ones-MacBook-Pro.local>
2016-01-19 11:18:33 +0200

    Added get more photos and more pages

--- api.js
+++ api.js
@@ -6,32 +6,36 @@
  Basic usage:
  
  window.fbAsyncInit = function () {
- fbapi.initialize();
- fbapi.isConnected();
- fbapi.login(function() {
- alert(fbapi.user.name);
- })           
+    fbapi.initialize();
+    fbapi.isConnected();
+    fbapi.login();
  }
  
  Maximum usage:
  
- window.fbAsyncInit = function () {
- fbapi.initialize();
- fbapi.isConnected();
- fbapi.login(function () {
- fbapi.getPages(function () {
- fbapi.setPhotosLimit(50)
- fbapi.pages[0].albums[0].getPhotos(function (){
- fbapi.pages[0].albums[0].getNextPhotos()
- })
- })
- });
- }
+    window.fbAsyncInit = function () {
+        fbapi.initialize();
+        fbapi.isConnected();
+        fbapi.login(function () {
+            fbapi.getUser(function () {
+                fbapi.getPages(function () {
+                    fbapi.pages[0].albums[0].getPhotos(function () {
+                        if(fbapi.pages[0].albums[0].hasMorePhotos()){
+                            fbapi.pages[0].albums[0].getNextPhotos();
+                        }
+                    });
+                    if(fbapi.hasMorePages()){
+                        fbapi.getMorePages();
+                    }
+                });
+            });
+        });
+    };
  
  Change debug variable to disable debug and info logging.
  FBapi imports Facebook SDK, do not import it again.
+ Make sure to add an application ID to your settings.
  */
-
 
 (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
@@ -51,7 +55,7 @@ var fbapi = {
     userID: null,
     user: null,
     pages: null,
-    __pages: null,
+    __pages: {next: null, prev: null, more: true}, // Holds pages related information for SDK use
     photos_limit: 25,
     initialize: function () {
         FB.init({
@@ -107,31 +111,46 @@ var fbapi = {
                 'GET',
                 function (response) {
                     fbapi.log(response, 'info');
-                    fbapi.user = new User(response);
+                    fbapi.user = new fbapi.User(response);
 
                     if (callback)
                         callback();
                 }
         );
     },
-    getPages: function (callback) {
+    getPages: function (callback, cursor) {
+        var query = 'me/accounts?fields=likes,name,talking_about_count,picture,photos,albums,access_token&limit=25'
+        
+        if(cursor)
+            query += "&after=" + cursor;
+            
         return FB.api(
-                'me/accounts?fields=likes,name,talking_about_count,picture,photos,albums,access_token',
+                query,
                 'GET',
                 function (response) {
                     fbapi.log(response, 'info');
                     fbapi.pages = [];
 
                     for (var key in response.data) {
-                        fbapi.pages.push(new Page(response.data[key]));
+                        fbapi.pages.push(new fbapi.Page(response.data[key]));
                     }
 
-                    fbapi.__pages = response.data
+                    if (response.paging) {
+                        fbapi.__pages.more = fbapi.pages.length === 25;
+                        fbapi.__pages.next = response.paging.cursors.after
+                        fbapi.__pages.prev = response.paging.cursors.before
+                    }
 
                     if (callback)
                         callback();
                 }
         );
+    },
+    getNextPages: function (callback) {
+        fbapi.getPages(callback, fbapi.__pages.next)
+    },
+    hasMorePages: function (callback) {
+        return fbapi.__pages.more
     },
     setPhotosLimit: function (limit) {
         fbapi.photos_limit = limit
@@ -150,60 +169,63 @@ var fbapi = {
                 if (debug)
                     console.log(data);
         }
+    },
+    Page: function (obj) {
+
+        this.name = obj.name;
+        this.thumb = obj.picture.data.url
+        this.albums = [];
+
+        for (var key in obj.albums.data) {
+            this.albums.push(new fbapi.Album(obj.albums.data[key]));
+        }
+
+    },
+    Album: function (obj) {
+
+        this.id = obj.id
+        this.name = obj.name
+        this.photos = []
+        this.pagination = {next: null, previous: null, more: null}
+
+    },
+    Photo: function (obj) {
+
+        this.id = obj.id
+        this.name = obj.name
+        this.source = obj.source
+        this.thumb = obj.picture
+
+    },
+    User: function (obj) {
+
+        this.id = obj.id
+        this.name = obj.name
+        this.picture = obj.picture.data.url
+
     }
-}
+};
 
-function Page(obj) {
-
-    this.name = obj.name;
-    this.thumb = obj.picture.data.url
-    this.albums = [];
-
-    for (var key in obj.albums.data) {
-        this.albums.push(new Album(obj.albums.data[key]));
-    }
-
-}
-
-function Album(obj) {
-
-    this.id = obj.id
-    this.name = obj.name
-    this.photos = []
-    this.next_photos = null
-    this.previous_photos = null
-
-}
-
-function Photo(obj) {
-
-    this.id = obj.id
-    this.name = obj.name
-    this.source = obj.source
-    this.thumb = obj.picture
-
-}
-
-Album.prototype.getPhotos = function (callback, cursor) {
+fbapi.Album.prototype.getPhotos = function (callback, cursor) {
     var self = this;
 
     var query = this.id + '/photos?fields=name,source,picture&limit=' + fbapi.photos_limit
     if (cursor)
-        query += '&after=' + cursor
+        query += '&after=' + cursor;
 
-    FB.api(query
-            ,
+    FB.api(query,
             'GET',
             function (response) {
                 self.photos = []
 
                 for (var key in response.data) {
-                    self.photos.push(new Photo(response.data[key]));
+                    self.photos.push(new fbapi.Photo(response.data[key]));
                 }
 
                 if (response.paging) {
-                    self.next_photos = response.paging.cursors.after
-                    self.previous_photos = response.paging.cursors.before
+                    self.pagination.more = self.photos.length === fbapi.photos_limit;
+                    self.pagination.next = response.paging.cursors.after
+                    self.pagination.previous = response.paging.cursors.before
                 }
 
                 fbapi.log(response, 'info')
@@ -212,19 +234,15 @@ Album.prototype.getPhotos = function (callback, cursor) {
                     callback()
             }
     );
-}
+};
 
-function User(obj) {
+fbapi.Album.prototype.getNextPhotos = function (callback) {
+    this.getPhotos(callback, this.pagination.next);
+};
 
-    this.id = obj.id
-    this.name = obj.name
-    this.picture = obj.picture.data.url
-
-}
-
-Album.prototype.getNextPhotos = function (callback) {
-    this.getPhotos(callback, this.next_photos)
-}
+fbapi.Album.prototype.hasMorePhotos = function () {
+    return this.pagination.more;
+};
 
 window.fbAsyncInit = function () {
     fbapi.initialize();
@@ -233,9 +251,11 @@ window.fbAsyncInit = function () {
         fbapi.getUser(function () {
             fbapi.getPages(function () {
                 fbapi.pages[0].albums[0].getPhotos(function () {
-                    fbapi.pages[0].albums[0].getNextPhotos()
-                })
-            })
-        })
+                    if(fbapi.pages[0].albums[0].hasMorePhotos()){
+                        fbapi.pages[0].albums[0].getNextPhotos();
+                    }
+                });
+            });
+        });
     });
-}
+};
